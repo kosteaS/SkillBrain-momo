035f174e357257555c7d8704f5c7cf6681b16ca1
kostea <costea88@mail.ru>
2024-10-28 19:43:12 +0200

    s-a adaugate tema 1 2 si 3

--- sprint7-js-intro/tema1.js
+++ sprint7-js-intro/tema1.js
@@ -1,1 +1,4 @@
-console.log("hello sprint7");
+let input = 100;
+let DivizibilCu10 = input % 10 === 0;
+
+console.log(DivizibilCu10);
---
+++ sprint7-js-intro/tema2.js
@@ -0,0 +1,8 @@
+function tellFortune(numarCopii, numePartener, locatieGeografica, locMunca) {
+    console.log(`Vei fi un ${locMunca} în ${locatieGeografica}, căsătorit cu ${numePartener} și vei avea ${numarCopii} copii.`);
+}
+tellFortune(
+    numarCopii=3, 
+    numePartener="Emanuel", 
+    locatieGeografica="Italia", 
+    locMunca="Programator");
